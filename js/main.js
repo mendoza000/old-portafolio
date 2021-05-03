34c215b835aa98ce79d7d06dceb2096f30847238
mendoza000 <omarmendoza.alv25@gmail.com>
2021-05-03 11:48:30 -0400

    Nombres de proyectos

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 const bars = document.querySelector('.nav_list');
 const barsButton = document.querySelector('#bars-button');
+const edad = document.getElementById('edad');
+
+let i = 1;
 let view = false
 
 console.log("inciando js...");
@@ -16,3 +19,14 @@ function hideShow() {
 		view = false
 	}
 }
+function myLoop () { 
+	setTimeout(function () { 
+		/*edad.innerText = `${i}`;*/
+		i++;
+		if (i < 18) {
+			myLoop();
+		}
+	}, 500)
+}
+
+myLoop();
