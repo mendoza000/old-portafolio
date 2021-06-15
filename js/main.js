b78a52e0c63fb894c38dd78ab27fdd2064fbdbb7
mendoza000 <omarmendoza.alv25@gmail.com>
2021-06-14 23:46:48 -0400

    about2: animacion de incremento

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 const bars = document.querySelector('.nav_list');
 const barsButton = document.querySelector('#bars-button');
 const edad = document.getElementById('edad');
-
-let i = 1;
+const project = document.getElementById('project');
+const leng = document.getElementById('leng');
 let view = false
 
 console.log("inciando js...");
@@ -19,15 +19,77 @@ function hideShow() {
 		view = false
 	}
 }
-function myLoop () { 
-	setTimeout(function () { 
-		/*edad.innerText = `${i}`;*/
-		i++;
-		if (i < 18) {
-			myLoop();
-		}
-	}, 500)
+
+/*cuando se ve, se activa la funcion*/
+let options = {
+	/*root: edad,
+	rootMargin: '0px',*/
+	threshold: 1
 }
 
+let x = 1;
 
-myLoop();
+function edadPlus() {
+	setTimeout(function() {
+		edad.innerText = `${x}`;
+		if (x < 17) {
+			x++;
+			edadPlus();
+		}
+	}, 200)
+}
+
+function edadCallback(entries, observer) {
+
+	if (entries[0].isIntersecting) {
+		edadPlus()
+	}
+
+}
+const observerEdad = new IntersectionObserver(edadCallback, options)
+observerEdad.observe(edad)
+
+
+let y = 1;
+
+function lengPlus() {
+	setTimeout(function() {
+		leng.innerText = `${y}`;
+		if (y < 4) {
+			y++;
+			lengPlus();
+		}
+	}, 200)
+}
+
+function lengCallback(entries, observer) {
+
+	if (entries[0].isIntersecting) {
+		lengPlus()
+	}
+}
+const observerLeng = new IntersectionObserver(lengCallback, options)
+observerLeng.observe(leng)
+
+
+
+let z = 1;
+
+function projectPlus() {
+	setTimeout(function() {
+		project.innerText = `${z}`;
+		if (z < 4) {
+			z++;
+			projectPlus();
+		}
+	}, 200)
+}
+
+function projectCallback(entries, observer) {
+
+	if (entries[0].isIntersecting) {
+		projectPlus()
+	}
+}
+const observerProject = new IntersectionObserver(projectCallback, options)
+observerProject.observe(project)
